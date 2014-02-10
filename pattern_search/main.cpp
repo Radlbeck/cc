cdb3610053b167a9ac0a6092326f974e44c18502
Radlbeck <radlbeck@live.com>
2014-02-09 20:51:41 -0500

    started file io code

--- pattern_search/main.cpp
+++ pattern_search/main.cpp
@@ -5,9 +5,25 @@
 #include <string>
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
-	
+	if(argc != 2){
+		cout << "No input file specified" << endl;
+		return 1;
+	}
+
+	ifstream file (argv[1]);
+	string line;
+	if(file.is_open()){
+		while(getline (file,line)){
+			cout << line << endl;
+		}
+		file.close();
+	}else{
+		cout << "File cannot be opened" << endl;
+		return 1;
+	}
+
 	return 0;
 }
 
