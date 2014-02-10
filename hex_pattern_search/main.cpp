24bb4e805af952856f2fcab668061e77530b9581
Radlbeck <radlbeck@live.com>
2014-02-09 23:18:02 -0500

    added patterns,works!, updated .gitignore

--- hex_pattern_search/main.cpp
+++ hex_pattern_search/main.cpp
@@ -15,21 +15,29 @@ int main(int argc, char* argv[])
 	// print/search file contents
 	FILE * pfile;
 	int current_bit = 0;
-	int pattern_count_aa = 0;
-	int pattern_count_55 = 0;
+	int pattern_aa = 0xaa;
+	int pattern_55 = 0x55;
+	int count_aa = 0;
+	int count_55 = 0;
 
 	pfile = fopen(argv[1], "r");
 
 	if(pfile != NULL){
 		while(current_bit != EOF){
 			current_bit = fgetc(pfile);
-			cout << current_bit << endl;
+			if(current_bit == pattern_aa)
+				count_aa++;
+			if(current_bit == pattern_55)
+				count_55++;
 		}
 		fclose(pfile);
 	}else{
 		cout << "File cannot be opened" << endl;
 		return 1;
 	}
+
+	cout << "count 0x55: " << count_55 << endl;
+	cout << "count 0xaa: " << count_aa << endl;
 
 	return 0;
 }
