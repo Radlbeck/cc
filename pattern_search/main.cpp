3496781e71c09e1bd802e043bd8ba0fe26c68d83
Radlbeck <radlbeck@live.com>
2014-02-09 21:24:16 -0500

    added TODOs

--- pattern_search/main.cpp
+++ pattern_search/main.cpp
@@ -7,15 +7,18 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2){
+	if(argc != 2){						// get file name
 		cout << "No input file specified" << endl;
 		return 1;
 	}
 
-	ifstream file (argv[1]);
+	//TODO parse file into hex (use hexdump?)
+
+
+	ifstream file (argv[1], ios::binary);			// print/search file contents
 	string line;
 	if(file.is_open()){
-		while(getline (file,line)){
+		while(getline (file,line)){	//TODO find patters in 'line'
 			cout << line << endl;
 		}
 		file.close();
