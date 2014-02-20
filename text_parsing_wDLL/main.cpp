2064b65a13f8fe3879765e45667700233b4620fd
Radlbeck <radlbeck@live.com>
2014-02-19 21:47:55 -0500

    removed debugging code; works!

--- text_parsing_wDLL/main.cpp
+++ text_parsing_wDLL/main.cpp
@@ -1,5 +1,9 @@
-// Code for basic data manipulation
-//
+/*
+ *  Lab 0 - Basic data and file manipulation
+ *
+ *	By: Andrew Radlbeck & John Wood
+ */
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -23,7 +27,7 @@ void printDLL_forward(node * head)
 		cout << current->word << ", ";  // print out the current word
 		current = current->next;	// set the next node to be the current
 	}
-	cout << current->word << endl;
+	cout << current->word << endl;		// print last node
 }
 
 node * getWords(char * infile)
@@ -35,7 +39,6 @@ node * getWords(char * infile)
 	if(file.is_open()){
 		string word;
 		while(file >> word){							// stream operator parses on white space (seperating words)
-			cout << "on word: " << word << endl;//TODO REMOVE
 			transform(word.begin(), word.end(), word.begin(), ::tolower);	// make sure all words are lower case
 
 			node * current = new node;		// create a new node pointer & allocate memory
@@ -44,49 +47,40 @@ node * getWords(char * infile)
 
 			strcpy(current->word, word.c_str());	// copy the lower case word into the node
 
-			while(find != NULL){
-				if((string)find->word < (string)current->word){
-					cout << "\t" << find->word << " < " << current->word << endl; //TODO REMOVE
-					prev = find;
+			while(find != NULL){						// loop threw the current DLL
+				if((string)find->word < (string)current->word){		// search untill word finds a higher word (alpebetically)
+					prev = find;					// increment pointer
 					find = find->next;
-				}else if((string)find->word == (string)current->word){
+				}else if((string)find->word == (string)current->word){	// ignore repeat words
 					dbreak = 1;
-					delete current;
+					delete current;					// deallocate memory
 					break;
 				}else{
-					break;
+					break;						// once location is found leave the loop
 				}
 			}
 
-			if(dbreak) continue;
+			if(dbreak) continue;						// if duplicate skip current word
 			if(prev != NULL){
-			       	prev->next    = current;		// set next, prev pointers
+			       	prev->next    = current;		// set next, prev pointers to fit diagram below
 				current->prev = prev;
 
-				current->next = find;
+				current->next = find;			// [prev] <-> [current] <-> [find]
 				find->prev    = current;
-				cout << "\t\t\t" << "link" << endl;	//TODO REMOVE
 			}else{
-				if(head == NULL){
+				if(head == NULL){			// if list is empty create head
 				       	head = current;
-					cout << "\t\t\t" << "head" << endl;	//TODO REMOVE
 				}else{
-					current->next = head;
+					current->next = head;		// if the word replaces the head adjust pointers
 					head->prev = current;
-					head = current;
-
-
-
-					cout << "\t\t\t" << "p head" << endl; 	//TODO REMOVE
+					head = current;			// [current] <-> [head]
 				}
 			}
-			printDLL_forward(head);	//TODO REMOVE
-
 		}
-		file.close();
+		file.close();					// close file
 	}else{
-		cout << ERROR << "cannot open file" << endl;
-		return NULL;
+		cout << ERROR << "cannot open file" << endl;	// if file open fails print an error
+		return NULL;			
 	}
 	return head;
 }
@@ -96,14 +90,14 @@ void printDLL_backward(node * head)
 {
 	node * current = head;
 	while(current->next != NULL){		// find the tail
-		current = current->next;
+		current = current->next;	// increment position pointer
 	}
 
-	while(current->prev != NULL){		// reversed print forward
+	while(current->prev != NULL){		// reversed printDLL_forward
 		cout << current->word << ", ";
-		current = current->prev;
+		current = current->prev;	// decrement position pointer
 	}
-	cout << current->word << endl;
+	cout << current->word << endl;		// print first word
 }
 
 void remove2ndNode(node * head)
@@ -119,11 +113,11 @@ void remove2ndNode(node * head)
 int main(int argc, char * argv[])
 {
 	// get file input
-	if(argc != 2){
+	if(argc != 2){				// Check for corrent number of parameters
 		cout << ERROR << "Please include file to open" << endl;
-		return -1;
+		return -1;			// return with an error condition
 	}
-	node * head = getWords(argv[1]);
+	node * head = getWords(argv[1]);	// get DLL from text file
 	
 	cout << "Test 1: Words in order" << endl;
 	printDLL_forward(head);
@@ -131,12 +125,12 @@ int main(int argc, char * argv[])
 	cout << endl << "Test 2: Words in reverse order" << endl;
 	printDLL_backward(head);
 	
-/*	cout << endl << "Test 3: Remove the 2nd item" << endl;
+	cout << endl << "Test 3: Remove the 2nd item" << endl;
 	remove2ndNode(head);
 
 	cout <<"Test 4: Words in order" << endl;
 	printDLL_forward(head);
-*/
+
 	return 0;
 }
 
