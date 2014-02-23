69ed2aa04aa4dcbb925a87788f5c43bb6e7f63c1
Radlbeck <radlbeck@live.com>
2014-02-23 14:13:09 -0500

    added template code

---
+++ multiprogramming/main.cpp
@@ -0,0 +1,13 @@
+/*
+ *  Lab 1 - Basic multiprogramming
+ *
+ *	By: Andrew Radlbeck & John Wood
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+
+	return 0;
+} 
