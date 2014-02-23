d2e98745efde2eb7aa785c02576270dcf100fa80
Radlbeck <radlbeck@live.com>
2014-02-23 15:28:56 -0500

    2nd part; needs testing

--- fork_basic/main.cpp
+++ fork_basic/main.cpp
@@ -1,5 +1,5 @@
 /*
- *  Pre-Lab 1 - Basic multiprogramming
+ *  Lab 1 - Basic multiprogramming
  *
  *	By: Andrew Radlbeck & John Wood
  */
@@ -9,17 +9,24 @@
 #include <stdlib.h>
 using namespace std;
 
-int main(int argc, char * argv[])
+int main(int argc, char* argv[])
 {
-
+	if(argc != 2){										// make sure correct parameter amount is entered
+		printf("Error: %s [ls/emacs]\n", argv[0]);		// if not inform user of usage
+		printf("		  ^specify cmd\n");		
+		return -1;										// exit with an error
+	}
+	char* cmd = argv[1]; //	save cmd
 	pid_t cid = fork();  // create child
 
 	if(cid == 0){		 // is child process
 		sleep(300);		 
-		_exit(0);		 // exit: _exit() for child
+		execlp(cmd, cmd, NULL);   // replace current procces with 'cmd'
+		_exit(0);		          // exit: _exit() for child
 	}else{				 // is parent process
 		sleep(200);		 
 		exit(0);		 
 	}
+
 	return 0;
 } 
