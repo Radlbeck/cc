0c831d2e9baab0ad69b2ed34e340c2ec50b3032c
Radlbeck <radlbeck@live.com>
2014-02-23 15:16:28 -0500

    completed part 1

--- fork_basic/main.cpp
+++ fork_basic/main.cpp
@@ -1,13 +1,25 @@
 /*
- *  Lab 1 - Basic multiprogramming
+ *  Pre-Lab 1 - Basic multiprogramming
  *
  *	By: Andrew Radlbeck & John Wood
  */
-#include <iostream>
+#include <cstdio>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
 using namespace std;
 
 int main(int argc, char * argv[])
 {
 
+	pid_t cid = fork();  // create child
+
+	if(cid == 0){		 // is child process
+		sleep(300);		 
+		_exit(0);		 // exit: _exit() for child
+	}else{				 // is parent process
+		sleep(200);		 
+		exit(0);		 
+	}
 	return 0;
 } 
