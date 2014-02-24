b6be429492d8cbc4ca0fd9ff67c214182155aeeb
Radlbeck <radlbeck@live.com>
2014-02-24 17:48:44 -0500

    added 2nd child; works with grep, but not more?

--- pipline_tasks/main.cpp
+++ pipline_tasks/main.cpp
@@ -22,7 +22,7 @@ using namespace std;
 
 int parse_user_commands(char *cmds[][MAX_ARGS], char *input, int *cmd_amount, int *arg_amount);
 int child_first(char *cmd[], int pipefd[]);
-int child_last(char *cmd[], int pipefd[]);
+int child_last(char *cmd[], int next_pipe[], int prev_pipe[]);
 
 int main(int argc, char *argv[])
 {
@@ -41,14 +41,19 @@ int main(int argc, char *argv[])
 
 	// setup process
 	int pipefd[2];		// create pipe file descripters
-	pid_t cid;
+	int pipefd_b[2];		// create pipe file descripters	
 
 	if(pipe(pipefd) == -1){							// handle pipe errors
 		printf("Error: could not open pipe\n");
 		return -1;
 	}
 
-	cid = fork();
+	if(pipe(pipefd_b) == -1){							// handle pipe errors
+		printf("Error: could not open pipe\n");
+		return -1;
+	}
+
+	pid_t cid = fork();
 	if(cid == -1){									// handle fork errors
 		printf("Error: could not create child\n");
 		return -1;	
@@ -56,16 +61,31 @@ int main(int argc, char *argv[])
 
 	if(cid == 0){	// in child
 		child_first(cmds[0], pipefd);
+
 	}else{			// in parent
-		close(pipefd[1]);	 		// close write end
 		waitpid(cid, 0, 0);
-		char buf;	// reads bit-by-bit from pipe to stdout
+		pid_t cid1 = fork();
 
-		while(read(pipefd[0], &buf, 1)){
-			write(1, &buf, 1);
-		}printf("\n");	
+		if(cid1 == -1){									// handle fork errors
+			printf("Error: could not create child\n");
+			return -1;	
+		}
 
-		close(pipefd[0]);		// close pipe
+		if(cid1 == 0){
+			child_last(cmds[1], pipefd_b, pipefd);
+		}else{
+			close(pipefd_b[1]);	 		// close write end
+			close(pipefd[0]);
+			close(pipefd[1]);
+			waitpid(cid1, 0, 0);
+			char buf;	// reads bit-by-bit from pipe to stdout
+
+			while(read(pipefd_b[0], &buf, 1)){
+				write(1, &buf, 1);
+			}printf("\n");	
+
+			close(pipefd_b[0]);		// close pipe
+		}				
 	}
 
 	return 0;
@@ -80,9 +100,11 @@ int child_first(char *cmd[], int pipefd[]){
 	return 0;
 }
 
-int child_last(char *cmd[], int pipefd[]){	
-	dup2(pipefd[0], 0);		// copys pipes read end into stdin [pipe < stdin]
-	dup2(pipefd[1], 1);		// copys pipes write end into stdout [stdout > pipe]
+int child_last(char *cmd[], int next_pipe[], int prev_pipe[]){
+	close(prev_pipe[1]);
+	close(next_pipe[0]);
+	dup2(prev_pipe[0], 0);		// copys pipes read end into stdin [pipe < stdin]
+	dup2(next_pipe[1], 1);		// copys pipes write end into stdout [stdout > pipe]
 
 	execvp(cmd[0], cmd);								// replace child with cmd		//TODO this needs to be execvp
 	printf("Error: failed to launch %s\n", cmd[0]);	// if failed the code will continue
