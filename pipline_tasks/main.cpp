c5fbccbd83d763caa1b4b2cb6f175ec10667cc26
Radlbeck <radlbeck@live.com>
2014-02-24 16:25:03 -0500

    started multi-fork code, updated readme

--- pipline_tasks/main.cpp
+++ pipline_tasks/main.cpp
@@ -2,16 +2,27 @@
  *  Lab 1 - Basic multiprogramming
  *
  *	By: Andrew Radlbeck & John Wood
+ *
+ *
+ * TODO:
+ *			-- change errors to redirect to stderr
+ *			
+ *
  */
 #include <cstdio>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 using namespace std;
 
 #define MAX_CMDS 2		//TODO upgrade to handle more commands
-#define MAX_BUF  1024   //TODO I have no idea if this is enough
+#define MAX_ARGS 5
+
+int parse_user_commands(char *cmds[][MAX_ARGS], char *input, int *cmd_amount, int *arg_amount);
+int child_first(char *cmd[], int pipefd[]);
+int child_last(char *cmd[], int pipefd[]);
 
 int main(int argc, char *argv[])
 {
@@ -22,37 +33,14 @@ int main(int argc, char *argv[])
 		return -1;										    // exit with an error
 	}
 
-	// get cmds from user input
-	char *str, *token, *saveptr;		//TODO sub tokenize to handle command arguments
-	char *cmds[MAX_CMDS];
-	int i;
-	for(i = 1, str = argv[1]; ; i++, str = NULL){ 	// 'str = NULL' - resume last spot of the string
-		token = strtok_r(str, "|", &saveptr);		// get pointer for next token
-		if(token == NULL) break;		
-
-		//TODO optimize - consider only moving pointers instead of allocating more memory MUST for args to work
-		int j, k;
-		int len = strlen(token);
-		char *token_trim = new char;
-		for(j = 0, k = 0; j < len; j++){
-			if(!isspace(token[j])){						
-				token_trim[k++] = token[j];				
-			}
-		}
-
-		// save commands into an array
-		if(i <= MAX_CMDS){
-			cmds[(i - 1)] = token_trim;			
-		}else{
-			printf("Error: too many commands\n");
-			printf("\t\t MAX COMMANDS = %i\n", MAX_CMDS);
-			return -1;
-		}
-	}
-	printf("%s, %s\n", cmds[0], cmds[1]); //TODO REMOVE
+	// parse user input
+	int  cmd_amount, arg_amount;
+	char *cmds[MAX_CMDS][MAX_ARGS];
+	if(parse_user_commands(cmds, argv[1], &cmd_amount, &arg_amount)) return -1;
+		
 
 	// setup process
-	int pipefd[2];			// create pipe file descripters
+	int pipefd[2];		// create pipe file descripters
 	pid_t cid;
 
 	if(pipe(pipefd) == -1){							// handle pipe errors
@@ -67,21 +55,72 @@ int main(int argc, char *argv[])
 	}
 
 	if(cid == 0){	// in child
-		close(pipefd[0]); 		// close read end
-		dup2(pipefd[1], 1);		// copys pipes write end into stdout [stdout > pipe]
-
-		execlp(cmds[0], cmds[0], NULL);						// replace child with cmd		
-		printf("Error: failed to launch %s\n", cmds[0]);	// if failed the code will continue
+		child_first(cmds[0], pipefd);
 	}else{			// in parent
-		close(pipefd[1]); 		// close write end
-
+		close(pipefd[1]);	 		// close write end
+		waitpid(cid, 0, 0);
 		char buf;	// reads bit-by-bit from pipe to stdout
+
 		while(read(pipefd[0], &buf, 1)){
 			write(1, &buf, 1);
-		}printf("\n");
+		}printf("\n");	
 
 		close(pipefd[0]);		// close pipe
 	}
 
 	return 0;
 } 
+
+int child_first(char *cmd[], int pipefd[]){
+	close(pipefd[0]); 		// close read end
+	dup2(pipefd[1], 1);		// copys pipes write end into stdout [stdout > pipe]
+
+	execvp(cmd[0], cmd);								// replace child with cmd		//TODO this needs to be execvp
+	printf("Error: failed to launch %s\n", cmd[0]);	// if failed the code will continue
+	return 0;
+}
+
+int child_last(char *cmd[], int pipefd[]){	
+	dup2(pipefd[0], 0);		// copys pipes read end into stdin [pipe < stdin]
+	dup2(pipefd[1], 1);		// copys pipes write end into stdout [stdout > pipe]
+
+	execvp(cmd[0], cmd);								// replace child with cmd		//TODO this needs to be execvp
+	printf("Error: failed to launch %s\n", cmd[0]);	// if failed the code will continue
+	return 0;
+}
+
+int parse_user_commands(char *cmds[][MAX_ARGS], char *input, int *cmd_amount, int *arg_amount)
+{
+	// TODO to add bonous part 2 insert another tokenizer to catch ">,<,<<,>>"
+	//	- add file redirection code to pipe as well
+	// get cmds from user input
+	char *str, *token, *saveptr;					//TODO sub tokenize to handle command arguments
+	char *sub_str, *sub_token, *sub_saveptr;	
+	int i, j;
+	for(i = 1, str = input; ; i++, str = NULL){ 	// 'str = NULL' - resume last spot of the string
+		token = strtok_r(str, "|", &saveptr);		// get pointer for next token
+		if(token == NULL) break;		
+
+		for(j = 1, sub_str = token; ;j++, sub_str=NULL){ 		// sub tokenize to find arguments for commands
+			sub_token = strtok_r(sub_str, " ", &sub_saveptr);			
+
+			if(i <= MAX_CMDS){
+				if(j <= (MAX_ARGS + 1)){
+					cmds[(i - 1)][(j - 1)] = sub_token;
+					//printf("%i, %i, %s\n", i, j, cmds[(i - 1)][(j - 1)]); //TODO REMOVE
+				}else{
+					printf("Error: too many arguments\n");
+					return -1;
+				}
+			}else{
+				printf("Error: too many commands\n");
+					return -1;
+			}
+			if(sub_token == NULL) break;					// we want the null in our argument array
+		}				
+	}
+	cmd_amount = &i;
+	arg_amount = &j;
+
+	return 0;
+}
